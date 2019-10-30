6a328ac0de9fe5521177744dc4620b9407e5c21f
Gregory Tang <gtang159@gmail.com>
2019-10-30 11:35:46 -0700

    Reinstalled textfit

---
+++ calculator/src/components/calculator/button/button.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { operators, specialOperators } from "../../../utils/constants";
+import "./button.css";
+
+export default ({ onButtonClick, buttonKey }) => {
+  let handleClick = e => {
+    onButtonClick(e.target.textContent);
+  };
+  let classNames = [
+    "btn",
+    operators.includes(buttonKey) ? "btn--orange" : "",
+    specialOperators.includes(buttonKey) ? "btn--grey" : "",
+    buttonKey === "0" ? "btn--zero" : ""
+  ];
+  return (
+    <button
+      name={buttonKey}
+      className={classNames.join(" ").trim()}
+      onClick={handleClick}
+    >
+      {buttonKey}
+    </button>
+  );
+};
---
+++ calculator/src/components/calculator/index.js
@@ -0,0 +1,9 @@
+import Screen from "./screen/screen";
+import Keypad from "./keypad/keypad";
+import Button from "./button/button";
+
+export default {
+  Screen,
+  Keypad,
+  Button
+};
